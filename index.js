09b7876115bdfa28f24ff43b603d42c0e588ee33
Prashik Patil <patil.prashik96@gmail.com>
2022-04-07 21:59:14 +0530

    Added route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+const express = require('express');
+const app = express();
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+app.listen(5000);
